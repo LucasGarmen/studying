2cf879f66f316a0184225a65153a06b0ff756013
LucasGarmen <lucasgarmendia95@gmail.com>
2025-11-26 15:24:12 -0300

    Arreglo backend y mejoras

--- backend.py
+++ backend.py
@@ -6,7 +6,10 @@ import re
 from difflib import SequenceMatcher
 from groq import Groq
 import os
+from dotenv import load_dotenv 
 
+
+load_dotenv() 
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
